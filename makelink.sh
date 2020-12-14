023add758f2142df6822b5360fd2ac35d78cba39
h173309 <h173309@edu.tut.ac.jp>
2020-12-15 05:53:46 +0900

    PATHを.*rcに移動

--- makelink.sh
+++ makelink.sh
@@ -1,6 +1,6 @@
-#!/bin/sh sh
+#!/usr/bin/sh
 
-for FILE in .aliases .bashrc .bash_logout .gitconfig .profile .zshrc; do 
+for FILE in .aliases .bashrc .bash_profile .bash_logout .gitconfig .profile .zshrc .zprofile; do 
     rm -f ~/${FILE}
     ln -s ~/dotfiles/${FILE} ~/${FILE}
 done
